3c4a2af7669d693220902ec81521954a48cee0bd
learner <roadoflearning@live.com>
2017-04-06 15:36:20 +0800

    撘出番茄钟页面的雏形

---
+++ app/src/main/java/cn/zheteng123/pomotodo/ui/countdown/CountdownActivity.java
@@ -0,0 +1,19 @@
+package cn.zheteng123.pomotodo.ui.countdown;
+
+import android.os.Bundle;
+
+import cn.zheteng123.pomotodo.R;
+import cn.zheteng123.pomotodo.ui.base.BaseActivity;
+
+public class CountdownActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_countdown;
+    }
+}
