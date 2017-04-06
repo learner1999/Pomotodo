c0b7a805a9103989ba17680458c8d76989c4109d
learner <roadoflearning@live.com>
2017-04-06 15:35:39 +0800

    添加 BaseActivity

---
+++ app/src/main/java/cn/zheteng123/pomotodo/ui/base/BaseActivity.java
@@ -0,0 +1,66 @@
+package cn.zheteng123.pomotodo.ui.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.zheteng123.pomotodo.R;
+
+/**
+ * <pre>
+ *     author : learner1999
+ *     e-mail : roadoflearning@live.com
+ *     time   : 2017/04/06
+ *     desc   : Activity 基类，在其中做一些初始化操作
+ *     version: 1.0
+ * </pre>
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.bind(this);
+
+        initToolbar();
+    }
+
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+
+            default:
+        }
+        return true;
+    }
+}
