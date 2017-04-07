c70464023e7bd258f54a0ffc40a0f791ffbc0caa
learner <roadoflearning@live.com>
2017-04-07 20:41:46 +0800

    新增添加 todo 任务

---
+++ app/src/main/java/cn/zheteng123/pomotodo/ui/addtodo/AddTodoActivity.java
@@ -0,0 +1,19 @@
+package cn.zheteng123.pomotodo.ui.addtodo;
+
+import android.os.Bundle;
+
+import cn.zheteng123.pomotodo.R;
+import cn.zheteng123.pomotodo.ui.base.BaseActivity;
+
+public class AddTodoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_add_todo;
+    }
+}
