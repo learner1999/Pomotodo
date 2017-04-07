051c0e792619d12bf2e074fd22d2ece39253e245
learner <roadoflearning@live.com>
2017-04-07 15:51:29 +0800

    添加 toolbar 菜单 添加任务按钮

--- app/src/main/java/cn/zheteng123/pomotodo/ui/todolist/TodoActivity.java
+++ app/src/main/java/cn/zheteng123/pomotodo/ui/todolist/TodoActivity.java
@@ -1,6 +1,7 @@
 package cn.zheteng123.pomotodo.ui.todolist;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import cn.zheteng123.pomotodo.R;
 import cn.zheteng123.pomotodo.ui.base.BaseActivity;
@@ -15,5 +16,11 @@ public class TodoActivity extends BaseActivity {
     @Override
     protected int getLayoutId() {
         return R.layout.activity_todo;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_todo, menu);
+        return true;
     }
 }
